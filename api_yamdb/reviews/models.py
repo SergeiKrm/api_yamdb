56416637d17786682b7237764eb6632bd6793a18
maria.subbotina <maria.subbotina@aramcoinnovations.com>
2023-05-23 10:01:31 +0300

    Models Review and Comment

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+
+
+CHOICES = [i for i in range(11)]
+
+
+class Title():
+
+
+class Review(models.Model):
+    #author...
+    title_id = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+    )
+    text = models.TextField()
+    score = models.IntegerField(choices=CHOICES)
+    pub_date = pub_date = models.DateTimeField(
+        'Дата публикации',
+        auto_now_add=True
+    )
+
+    def __str__(self) -> str:
+        return self.text
+
+
+class Comment(models.Model):
+    #author...
+    review_id = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='comments',
+    )
+    text = models.TextField()
+    pub_date = pub_date = models.DateTimeField(
+        'Дата публикации',
+        auto_now_add=True
+    )
+
+    def __str__(self) -> str:
+        return self.text
+
