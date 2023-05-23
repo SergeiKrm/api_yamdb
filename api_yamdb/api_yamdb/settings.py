0ae93d6c2d69163f347df2fb22b20e888d0195ea
Îëåã Åôðåìîâ <ole_f@mail.ru>
2023-05-23 14:58:58 +0500

    Фича 2 с устранением замечаний Марии

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -21,6 +21,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'django_filters',
     'reviews.apps.ReviewsConfig',
     'api.apps.ApiConfig',
     'users.apps.UsersConfig',
@@ -104,3 +106,17 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ],
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5,
+}
--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib import admin
+from .models import Category, Genre, Title
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'slug')
+    search_fields = ('name', 'slug')
+    empty_value_display = '-пусто-'
+    list_editable = ('name', 'slug')
+
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'slug')
+    search_fields = ('name', 'slug')
+    empty_value_display = '-пусто-'
+    list_editable = ('name', 'slug')
+
+
+class TitlesAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'year', 'category')
+    search_fields = ('name', 'category')
+    empty_value_display = '-пусто-'
+    list_editable = ('name', 'category')
