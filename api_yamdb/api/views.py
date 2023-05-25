8a9d0e8031f574f85272f649dc9de77a73141a1f
maria.subbotina <maria.subbotina@aramcoinnovations.com>
2023-05-25 20:37:01 +0300

    Permissions added

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
+from .permissions import (
+    IsAuthorOrReadOnly,
+    IsModeratorOrAdminOrSuperuserOrReadOnly
+)
 
 
 from reviews.models import Title, Review
@@ -11,7 +15,10 @@ from .serializers import (
 
 class ReviewViewSet(viewsets.ModelViewSet):
     serializer_class = ReviewSerializer
-    # permission_classes
+    permission_classes = (
+        IsAuthorOrReadOnly,
+        IsModeratorOrAdminOrSuperuserOrReadOnly,
+    )
 
     def get_queryset(self):
         title = get_object_or_404(Title, id=self.kwargs.get('title_id'))
@@ -26,7 +33,10 @@ class ReviewViewSet(viewsets.ModelViewSet):
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    # permission_classes
+    permission_classes = (
+        IsAuthorOrReadOnly,
+        IsModeratorOrAdminOrSuperuserOrReadOnly,
+    )
 
     def get_queryset(self):
         review = get_object_or_404(Review, pk=self.kwargs.get('review_id'))
