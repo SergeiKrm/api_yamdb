b26c886bc14699f6cb740d855520b65cc0b93382
mvs51 <83452416+mvs51@users.noreply.github.com>
2023-05-27 13:20:32 +0300

    Features/models (#9)
    * Permissions added
    
    * Permissions edited
    
    * Joint models file
    
    ---------
    
    Co-authored-by: maria.subbotina <maria.subbotina@aramcoinnovations.com>

---
+++ api_yamdb/api/permissions.py
@@ -0,0 +1,32 @@
+from rest_framework import permissions
+
+
+class IsAuthorOrReadOnly(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return (
+            request.user.is_authenticated
+            or request.method in permissions.SAFE_METHODS
+        )
+
+    def has_object_permission(self, request, view, obj):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or obj.author == request.user
+        )
+
+
+class IsModeratorOrAdminOrSuperuserOrReadOnly(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return (
+            request.user.is_authenticated
+            or request.method in permissions.SAFE_METHODS
+        )
+
+    def has_object_permission(self, request, view, obj):
+        return (
+            request.user.is_admin
+            or request.user.is_moderator
+            or request.user.is_superuser
+        )
