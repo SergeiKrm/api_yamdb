7584704d343c25d8afcfaa855e46fd2e47ba78b8
Sergei Kramarev <122915403+SergeiKrm@users.noreply.github.com>
2023-05-30 22:26:03 +0600

    Jwt (#16)
    * mail +
    
    * me +
    
    * функционал готов, но pytestы не все пройдены

--- api_yamdb/users/serializers.py
+++ api_yamdb/users/serializers.py
@@ -1,9 +1,19 @@
 from rest_framework import serializers
-from .models import User
+from rest_framework.validators import UniqueValidator
+
+from users.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
-    
+    username = serializers.CharField(
+        max_length=150,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    email = serializers.EmailField(
+        max_length=254,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+
     class Meta:
         model = User
         fields = (
@@ -15,9 +25,44 @@ class UserSerializer(serializers.ModelSerializer):
             'role'
         )
 
-    # Использовать имя 'me' в качестве username запрещено.
     def validate_username(self, value):
         if value == 'me':
             raise serializers.ValidationError(
                 'Использовать имя "me" в качестве username запрещено!')
         return value
+
+
+class SignUpSerialier(serializers.ModelSerializer):
+    username = serializers.CharField(       # RegexField(regex='^[\w.@+-]+\z',
+        max_length=150,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    email = serializers.EmailField(
+        max_length=254,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+        # добавить ограничение на username=me
+
+
+class TokenCreateSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    confirmation_code = serializers.CharField()
+
+
+class EditMyselfSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'email',
+            'first_name',
+            'last_name',
+            'bio',
+            'role'
+        )
+        read_only_fields = ('role',)
