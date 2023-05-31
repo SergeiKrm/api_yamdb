e19267ff1aa8c1d53622b95946b78fdcf58bfa9f
Sergei Kramarev <122915403+SergeiKrm@users.noreply.github.com>
2023-06-01 02:04:14 +0600

    all pytests + (#20)

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -30,3 +30,20 @@ class IsAdmin(permissions.BasePermission):
         return (request.user.is_authenticated
                 and (request.user.role == 'admin'
                      or request.user.is_superuser))
+
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return (request.user.is_authenticated
+                and (request.user.role == 'admin'
+                     or request.user.is_superuser))
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return (request.user.is_authenticated
+                and (request.user.role == 'admin'
+                     or request.user.is_superuser))
