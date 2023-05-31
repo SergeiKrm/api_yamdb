0662634035fbad9c5ff6b693453cf25cffb9227a
Sergei Kramarev <122915403+SergeiKrm@users.noreply.github.com>
2023-05-31 19:39:28 +0600

    User/pytest (#17)
    * pytset 00_registration +
    
    * все pytestы +
    
    * pytests ++

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -30,3 +30,16 @@ class IsModeratorOrAdminOrSuperuserOrReadOnly(permissions.BasePermission):
             or request.user.is_moderator
             or request.user.is_superuser
         )
+
+
+class IsAdmin(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return (request.user.is_authenticated
+                and (request.user.role == 'admin'
+                     or request.user.is_superuser))
+
+    def has_object_permission(self, request, view, obj):
+        return (request.user.is_authenticated
+                and (request.user.role == 'admin'
+                     or request.user.is_superuser))
--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,5 +1,7 @@
+from django.core.validators import RegexValidator
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
+from rest_framework.validators import UniqueValidator
 
 from reviews.models import (
     Title,
@@ -8,6 +10,7 @@ from reviews.models import (
     Review,
     Comment
 )
+from users.models import User
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -48,3 +51,94 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         fields = '__all__'
         model = Comment
+
+
+class UserSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(
+        max_length=150,
+        validators=[
+            UniqueValidator(queryset=User.objects.all()),
+            RegexValidator(
+                regex=r'^[\w.@+-]+$',
+                message='Имя пользователя содержит недопустимый символ'
+            )
+        ]
+    )
+    email = serializers.EmailField(
+        max_length=254,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'email',
+            'first_name',
+            'last_name',
+            'bio',
+            'role'
+        )
+
+    def validate_username(self, value):
+        if value == 'me':
+            raise serializers.ValidationError(
+                'Использовать имя "me" в качестве username запрещено!')
+        return value
+
+
+class SignUpSerialier(serializers.ModelSerializer):
+    username = serializers.CharField(
+        max_length=150,
+        validators=[
+            UniqueValidator(queryset=User.objects.all()),
+            RegexValidator(
+                regex=r'^[\w.@+-]+$',
+                message='Имя пользователя содержит недопустимый символ'
+            )
+        ]
+    )
+    email = serializers.EmailField(
+        max_length=254,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+
+    def validate_username(self, value):
+        if value == 'me':
+            raise serializers.ValidationError(
+                'Использовать имя "me" в качестве username запрещено!')
+        return value
+
+
+class TokenCreateSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    confirmation_code = serializers.CharField()
+
+
+class EditMyselfSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(
+        max_length=150,
+        validators=[
+            UniqueValidator(queryset=User.objects.all()),
+            RegexValidator(
+                regex=r'^[\w.@+-]+$',
+                message='Имя пользователя содержит недопустимый символ'
+            )
+        ]
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'email',
+            'first_name',
+            'last_name',
+            'bio',
+            'role'
+        )
+        read_only_fields = ('role',)
